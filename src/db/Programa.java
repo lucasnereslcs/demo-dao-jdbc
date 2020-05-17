01f69ca62cb51ff4bd4030a72d6998d4d3ad351b
lucasnereslcs <lucasnereslcs@gmail.com>
2020-05-17 16:35:29 -0300

    Sellers

--- src/db/Programa.java
+++ src/db/Programa.java
@@ -1,13 +1,20 @@
 package db;
 
+import java.util.Date;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
+		//SimpleDateFormat sfd = new SimpleDateFormat("dd/MM/yyyy");
+		
+		
 		
 		Department obj = new Department(1, "Books");
 		
-		System.out.println(obj);
+		Seller seller = new Seller(1, "bob", "bob@gmail.com", new Date(), 3000.0, obj  );
+		
+		System.out.println(seller);
 
 		
 	}

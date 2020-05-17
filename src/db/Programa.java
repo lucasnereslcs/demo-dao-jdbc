98858717564024ca179451033d690b09dd2d04cf
lucasnereslcs <lucasnereslcs@gmail.com>
2020-05-17 16:14:52 -0300

    Department

---
+++ src/db/Programa.java
@@ -0,0 +1,15 @@
+package db;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		
+		Department obj = new Department(1, "Books");
+		
+		System.out.println(obj);
+
+		
+	}
+
+}
